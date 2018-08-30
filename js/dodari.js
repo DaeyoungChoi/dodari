2944dd2540e3f9460fb0386c33b46bf0070aba5f
noah <noah@protopie.io>
2018-08-30 21:28:36 +0900

    noah

--- js/dodari.js
+++
@@ -1,9 +0,0 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCTopAppBar} from '@material/top-app-bar/index';
-import {MDCComponent, MDCFoundation} from '@material/base';
-
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
-iconButtonRipple.unbounded = true;
